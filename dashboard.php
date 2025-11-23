d4be49ab75910ce608f949718e0c4f67842b1c0f
auliarahma <auliarahma12082@gmail.com>
2025-11-23 15:15:15 +0700

    Dashboard Penjualan (Perhitungan Total)

--- dashboard.php
+++ dashboard.php
@@ -1,29 +1,7 @@
-$jumlah = [];
-$total = [];
-
-for ($i = 0; $i < count($kode_barang); $i++) {
-    $jumlah[$i] = rand(1,5);     // pembelian acak 1–5
-    $total[$i] = $jumlah[$i] * $harga_barang[$i];
+$grandtotal = 0;
+for ($i = 0; $i < count($total); $i++) {
+    $grandtotal += $total[$i];
 }
 
-<h3>Daftar Pembelian (Random)</h3>
-
-<table border="1" cellpadding="6">
-<tr>
-    <th>Kode</th>
-    <th>Nama Barang</th>
-    <th>Harga</th>
-    <th>Jumlah</th>
-    <th>Total</th>
-</tr>
-
-<?php for ($i = 0; $i < count($kode_barang); $i++) { ?>
-<tr>
-    <td><?= $kode_barang[$i] ?></td>
-    <td><?= $nama_barang[$i] ?></td>
-    <td><?= $harga_barang[$i] ?></td>
-    <td><?= $jumlah[$i] ?></td>
-    <td><?= $total[$i] ?></td>
-</tr>
-<?php } ?>
-</table>
+<h3>Total Belanja</h3>
+<p>Subtotal : Rp <?= number_format($grandtotal,0,',','.') ?></p>
