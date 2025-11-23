43de81e4f099ed7ac6db4467adb276b4df75f000
auliarahma <auliarahma12082@gmail.com>
2025-11-23 15:19:34 +0700

    Dashboard Penjualan (Diskon Belanja)

--- dashboard.php
+++ dashboard.php
@@ -1,2 +1,17 @@
-<h3>Total Belanja (Sebelum Diskon)</h3>
-<p><b>Rp <?= number_format($grandtotal,0,',','.') ?></b></p>
+$diskon = 0;
+
+if ($grandtotal < 50000) {
+    $diskon = 0.05 * $grandtotal;
+} elseif ($grandtotal <= 100000) {
+    $diskon = 0.10 * $grandtotal;
+} else {
+    $diskon = 0.15 * $grandtotal;
+}
+
+$total_bayar = $grandtotal - $diskon;
+
+<h3>Diskon Pembelian</h3>
+<p>Diskon: Rp <?= number_format($diskon,0,',','.') ?></p>
+
+<h3>Total Bayar Setelah Diskon</h3>
+<p><b>Rp <?= number_format($total_bayar,0,',','.') ?></b></p>
